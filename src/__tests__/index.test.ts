d60a6ce4c3e876250d30a39bcc47dbe1ce4a50d4
tkovs <joao.rodrigues@arapiraca.ufal.br>
2020-03-21 13:56:50 -0300

    Fix tests environment

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,8 +1,8 @@
 import { track } from '../index';
 
-describe('track', () => {
-  it('should mount a complete object with an valid code', () => {
-    const response = track('PW086958114BR');
-    expect(response).toEqual({});
+describe('Track', () => {
+  it('should mount the correct string with correct params', () => {
+    const response = track('mockCode');
+    expect(response).toEqual('Packet mockCode tracked. Thank you!');
   });
 });
