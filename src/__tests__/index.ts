4108fe89ed3915f4794ef6cb15d9168b03f98cbd
tkovs <joao.rodrigues@arapiraca.ufal.br>
2020-03-21 05:38:46 -0300

    Implement first version of track

--- src/__tests__/index.ts
+++
@@ -1,8 +0,0 @@
-import { track } from '../index';
-
-describe('Track', () => {
-  it('should mount the correct string with correct params', () => {
-    const response = track('mockCode');
-    expect(response).toEqual('Packet mockCode tracked. Thank you!');
-  });
-});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,31 @@
-export const track = (code: string) => `Packet ${code} tracked. Thank you!`;
+import cheerio from 'cheerio';
+import axios from 'axios';
+
+const url = (code: string) => `https://www.linkcorreios.com.br/?id=${code}`;
+
+export const track = async (code: string) => {
+  const response = await axios.get(url(code));
+  const { data } = response;
+  const html = cheerio.load(data);
+  const final: any[] = [];
+
+  html('.linha_status').each((_, statusElement) => {
+    const object: any = {};
+
+    html(statusElement)
+      .find('li')
+      .each((_, infoElement) => {
+        const text = html(infoElement).text();
+        const key: any = /^\w+/.exec(text);
+        const match = /:\s*(.*)$/.exec(text);
+
+        object[key] = match && match[1];
+      });
+
+    final.push(object);
+  });
+
+  return final;
+};
+
+track('PW086958114BR');
