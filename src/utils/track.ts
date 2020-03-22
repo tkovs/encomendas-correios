d64a01a7a03d7a401d2138dfb7fd95b2577b4b75
tkovs <joao.rodrigues@arapiraca.ufal.br>
2020-03-22 01:50:53 -0300

    Destructure locale string in small properties

--- src/utils/track.ts
+++ src/utils/track.ts
@@ -1,7 +1,24 @@
 export const getUrl = (code: string) => `https://www.linkcorreios.com.br/?id=${code}`
 
+const extractLocale = (value: string) => {
+  const regex = /(.*) - (.*) \/ (.*)/
+  const locales = regex.exec(value)
+
+  if (!locales) {
+    return
+  }
+
+  return {
+    place: locales[1],
+    city: locales[2],
+    state: locales[3],
+  }
+}
+
 const extractDate = (value: string) => {
-  const datetime = /(\d+)\/(\d+)\/(\d+).*?: (\d+):(\d+)/.exec(value)
+  const regex = /(\d+)\/(\d+)\/(\d+).*?: (\d+):(\d+)/
+  const datetime = regex.exec(value)
+
   if (!datetime) {
     return {}
   }
@@ -28,6 +45,21 @@ export const buildElement = (key: string, value: string) => {
       return {
         key: 'datetime',
         value: extractDate(value),
+      }
+    case 'origem':
+      return {
+        key: 'from',
+        value: extractLocale(value),
+      }
+    case 'destino':
+      return {
+        key: 'to',
+        value: extractLocale(value),
+      }
+    case 'local':
+      return {
+        key: 'locale',
+        value: extractLocale(value),
       }
     default:
       return {
